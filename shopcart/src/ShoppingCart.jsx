7f14e51a1ca088b958a0c118162b0723d13e42c1
= <avula.usedu1@gmail.com>
2026-03-12 19:03:49 -0500

    changes in the shopping cart

--- shopcart/src/ShoppingCart.jsx
+++ shopcart/src/ShoppingCart.jsx
@@ -2,21 +2,42 @@ import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import "./ShoppingCart.css";
 
 function ShoppingCart({ productData }) {
-
-
   let [quantity, setQuantity] = useState([0, 0, 0, 0]);
-
+  let [openPopUp, setopenPopUp] = useState(false);
+  let [selectedProduct, setSelectedProduct] = useState(null);
   let changedAmount = (e, idx) => {
     let copy = [...quantity];
     copy[idx] = e.target.value;
     setQuantity(copy);
   };
 
-    let totalNoOfItems = quantity.reduce((sum,curr)=>{
-      return sum=sum+Number(curr)
-    },0);
+  let totalNoOfItems = quantity.reduce((sum, curr) => {
+    return (sum = sum + Number(curr));
+  }, 0);
+  let addToCart = (e, idx) => {
+    let copyOfQty = [...quantity];
+    copyOfQty[idx] = copyOfQty[idx] + 1;
+    setQuantity(copyOfQty);
+  };
+  let deleteFromCart = (e, idx) => {
+    let copyOfQty = [...quantity];
+    copyOfQty[idx] = copyOfQty[idx] - 1;
+    if(copyOfQty[idx]<0){
+      copyOfQty[idx]=0
+    }
+    setQuantity(copyOfQty);
+  };
+  let showPopUp = (e) => {
+    setSelectedProduct(e);
+    setopenPopUp(true);
+  };
+  let closePopUp = () => {
+    setopenPopUp(false);
+    setSelectedProduct(null);
+  };
   return (
     <>
       <div className="m-2">
@@ -40,32 +61,82 @@ function ShoppingCart({ productData }) {
             style={{ border: "1px solid #bbb5b5" }}
             className="p-3 d-flex flex-row"
           >
-            <div className="d-flex flex-column  align-items-center" style={{width:"15rem"}}>
+            <div
+              className="d-flex flex-column  align-items-center"
+              style={{ width: "15rem" }}
+            >
               <h4>{el.desc}</h4>
-              <div>
+              <div onClick={(e) => showPopUp(el)}>
                 {" "}
                 <img src={el.image} style={{ height: "5rem", width: "5rem" }} />
               </div>
             </div>
+
             <div
               className="d-flex flex-row  align-items-center"
               style={{ marginTop: "3rem", marginLeft: "1rem" }}
             >
-              <div>
-                <label>
+              <div className="d-flex flex-row  ">
+                <div className="m-2  addBtnDiv">
+                  <button
+                    className="addBtn"
+                    onClick={(e) => addToCart(e, index)}
+                  >
+                    +
+                  </button>
+                </div>
+                <div className="m-2 addBtnDiv">
+                  <button
+                    className="addBtn"
+                    onClick={(e) => deleteFromCart(e, index)}
+                  >
+                    -
+                  </button>
+                </div>
+              </div>
+              <div className="d-flex flex-column" style={{marginTop:"-2rem", marginLeft:"1rem"}}>
+               <p style={{marginBottom:"9px"}}>Quantity</p>
+               <label>
                   <input
-                  autoComplete="off"
+                    autoComplete="off"
                     name="myInput"
                     value={quantity[index]}
                     onChange={(e) => changedAmount(e, index)}
-                    style={{width:"2rem",border:"3px solid #bbb5b5",height:"3rem",textAlign:"center"}}
+                    style={{
+                      width: "3rem",
+                      border: "3px solid #bbb5b5",
+                      height: "2.5rem",
+                      textAlign: "center",
+                    }}
                   />
                 </label>{" "}
-                {/* {totalNoOfItems} */}
-              </div>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <p>Quantity</p>
+                </div>
+             
             </div>
+            {openPopUp && selectedProduct && (
+              <div className="popupOverlay" onClick={closePopUp}>
+                <div className="popupBox" onClick={(e) => e.stopPropagation()}>
+                  <button className="closeBtn" onClick={closePopUp}>
+                    X
+                  </button>
+
+                  <h3 className="p-3">{selectedProduct.desc}</h3>
+                  <hr />
+                  <div style={{textAlign:"center"}}>
+                  <img
+                    src={selectedProduct.image}
+                    alt={selectedProduct.desc}
+                    style={{
+                      width: "200px",
+                      height: "200px",
+                      objectFit: "cover", textAlign:"center",justifyContent:"center"
+                    }}
+                  />
+                  </div>
+                  <p className="p-3">Ratings: 3.5/5</p>
+                </div>
+              </div>
+            )}
           </div>
         ))}
       </div>
